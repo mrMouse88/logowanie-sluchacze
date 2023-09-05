94ad81e86fd5d041c7c0503f404c57ca607acf70
mrMouse88 <ia@asygnata.com>
2023-09-05 13:05:44 +0200

    cleaning

--- app.js
+++ app.js
@@ -3,15 +3,13 @@ const xlsx = require("node-xlsx");
 
 var parsedFile = xlsx.parse("osoby.xlsx");
 var list = parsedFile[0].data;
-console.log(list);
+
 for (var i = 1; i < list.length; ++i) {
   list[i].push(generatePassword());
 }
 list[0].push("haslo");
-console.log(list);
 
 const buffer = xlsx.build([{ name: "Arkusz1", data: list }]);
-console.log(buffer);
 
 fs.writeFile("osoby-hasla.xlsx", buffer, (err) => {
   if (err) throw err;
